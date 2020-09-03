b784a9b95803f5491a9875ab76ab68259eb17c75
Christine Oo <ootjinvoon@gmail.com>
2020-09-03 22:15:34 +0900

    test

--- trainer/task.py
+++ trainer/task.py
@@ -3,7 +3,7 @@ import pandas as pd
 def main():
   dates = pd.date_range('20130101', periods=6)
   print(dates)
-  print('testing 12345, trigger circleci ~!')
+  print('testing 12345, trigger circleci ~!!')
 
 if __name__ == '__main__':
   main()
